2fd1a68fdc338ea27dc5f88fa3b6fecd9bd0b12b
Mounik1901 <mounikroyal1901@gmail.com>
2024-12-24 21:48:56 +0530

    Code and test cases for string calculator without explicit delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,18 @@ class StringCalculator
     def add(number_str)
         if(number_str == "")
             return 0
+        elsif(number_str.scan(/\d+\z/).length == 0)
+            return "Invalid Input given: #{number_str}"
+        else
+            numbers = number_str.scan(/-?\d+/)
+            numbers = numbers.map(&:to_i)
+            negative_numbers = numbers.select{|ele| ele < 0}
+            if(negative_numbers.length > 0)
+              raise "Negative numbers (#{negative_numbers.join(", ")}) are provided"
+            else
+              numbers_sum = numbers.select{|ele| ele < 1000}.sum
+              return numbers_sum
+            end
         end
     end
 end
--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -9,4 +9,26 @@ class TestStringCalculator < Minitest::Test
     def test_empty_string_input
         assert_equal 0, @string_calculator.add("")
     end
+
+    def test_invalid_input
+        assert_equal "Invalid Input given: 1*2*", @string_calculator.add("1*2*")
+        assert_equal "Invalid Input given: *", @string_calculator.add("*")
+    end
+
+    def test_with_negative_numbers_without_explicit_delimiter
+        error = assert_raises do
+            @string_calculator.add("-112")
+        end
+        assert_equal("Negative numbers (-112) are provided", error.message)
+        error = assert_raises do
+            @string_calculator.add("1*-2&9%4*-10&-90")
+        end
+        assert_equal("Negative numbers (-2, -10, -90) are provided", error.message)
+    end
+
+    def test_output_with_proper_input_without_explicit_delimiter
+        assert_equal 1, @string_calculator.add("1")
+        assert_equal 15, @string_calculator.add("1*2:3;4)5")
+        assert_equal 90, @string_calculator.add("//[20.20.50")
+    end
 end
