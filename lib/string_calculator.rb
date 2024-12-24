c3e83a9301ef0990d530aed217470eae937819d8
Mounik1901 <mounikroyal1901@gmail.com>
2024-12-24 22:08:14 +0530

    Code and test cases for string calculator with explicit behaviour

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,13 +5,18 @@ class StringCalculator
         elsif(number_str.scan(/\d+\z/).length == 0)
             return "Invalid Input given: #{number_str}"
         else
-            numbers = number_str.scan(/-?\d+/)
+            if(number_str.start_with?("//[")) && number_str.include?("]\n")
+                ds = number_str.scan(/\[(.*?)\]/).flatten.join()
+                numbers = number_str.split("\n")[1].split(/[#{ds}]/)
+            else
+                numbers = number_str.scan(/-?\d+/)
+            end
             numbers = numbers.map(&:to_i)
             negative_numbers = numbers.select{|ele| ele < 0}
             if(negative_numbers.length > 0)
               raise "Negative numbers (#{negative_numbers.join(", ")}) are provided"
             else
-              numbers_sum = numbers.select{|ele| ele < 1000}.sum
+              numbers_sum = numbers.select{|ele| ele <= 1000}.sum
               return numbers_sum
             end
         end
--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -31,4 +31,26 @@ class TestStringCalculator < Minitest::Test
         assert_equal 15, @string_calculator.add("1*2:3;4)5")
         assert_equal 90, @string_calculator.add("//[20.20.50")
     end
+
+    def test_output_with_proper_input_without_explicit_delimiter_with_numbers_greater_than_1000
+        assert_equal 1015, @string_calculator.add("1*2:3;4)5:1000")
+        assert_equal 15, @string_calculator.add("1*2:3;4)5:1001")
+    end
+
+    def test_with_negative_numbers_with_explicit_delimiter
+        error = assert_raises do
+            @string_calculator.add("//[@@@][;]\n10@@@-2@@@-4;-98;48")
+        end
+        assert_equal("Negative numbers (-2, -4, -98) are provided", error.message)
+    end
+
+    def test_output_with_proper_input_with_explicit_delimiter
+        assert_equal 6, @string_calculator.add("//[***]\n1***2***3")
+        assert_equal 11, @string_calculator.add("//[*][%]\n1*4%6")
+    end
+
+    def test_output_with_proper_input_with_explicit_delimiter_with_numbers_greater_than_1000
+        assert_equal 1011, @string_calculator.add("//[*][%]\n1*4%6%1000")
+        assert_equal 15, @string_calculator.add("//[*][:][;][)]\n1*2:3;4)5:1001")
+    end
 end
